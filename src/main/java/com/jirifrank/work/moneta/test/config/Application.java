4113bb5520fda997d4657e6c8e9e43ed8977b48d
JiriFrank <info@jirifrank.eu>
2017-08-22 11:26:50 +0200

    Renamed Config due to inconsitenci in CoC

--- src/main/java/com/jirifrank/work/moneta/test/config/Application.java
+++ src/main/java/com/jirifrank/work/moneta/test/config/Application.java
@@ -3,15 +3,15 @@ package com.jirifrank.work.moneta.test.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static com.jirifrank.work.moneta.test.config.ApplicationConfig.APPLICATION_PACKAGE;
+import static com.jirifrank.work.moneta.test.config.Application.APPLICATION_PACKAGE;
 
 @SpringBootApplication(scanBasePackages = APPLICATION_PACKAGE)
-public class ApplicationConfig {
+public class Application {
 
 	public static final String APPLICATION_PACKAGE = "com.jirifrank.work.moneta.test";
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicationConfig.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
